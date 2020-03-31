f9860d0f7d78ed1660899f1006221d8915ac5663
Curtis La Graff <curtis@lagraff.me>
2020-03-30 20:56:56 -0400

    Command registry (#2)
    * Update workflow for better testing
    
    * Add commands pkg
    
    * Ignore binaries in git ignore
    
    * Add task runner script

---
+++ commands/commands.go
@@ -0,0 +1,16 @@
+package commands
+
+// Command represents an executable cli sub/command.
+type Command interface {
+
+	// Name of the command
+	Name() string
+
+	// Execute the command's logic with the provided arguments and flags.
+	Exec([]string) error
+}
+
+// Commands returns a list of all available commands.
+func Commands() []Command {
+	return nil
+}
---
+++ commands/commands_test.go
@@ -0,0 +1,18 @@
+package commands
+
+import "testing"
+
+func TestCommands(t *testing.T) {
+	availableCommands := Commands()
+
+	expectedLen := 0
+	actualLen := len(availableCommands)
+
+	if actualLen != expectedLen {
+		t.Errorf(
+			"expected length: %d did not equal actual length: %d",
+			expectedLen,
+			actualLen,
+		)
+	}
+}
